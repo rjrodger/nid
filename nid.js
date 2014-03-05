6e0007cb6476c6b8f784d0b5e8ee37050f2fbd64
Joe Moorman <mindwalker@innerspace.net>
2014-03-05 15:35:59 -0600

    added variant of banned word without the c

--- nid.js
+++ nid.js
@@ -2,7 +2,7 @@
 "use strict";
 
 
-/* Module API: 
+/* Module API:
  * var nid = require('nid')
  * nid() // returns new random id as string
  * nid({options}) // returns new nid function that generates ids using options
@@ -21,127 +21,126 @@
 
 
 var defaults = {
-  length:   6,
-  alphabet: '0123456789abcdefghijklmnopqrstuvwxyz',
+    length: 6,
+    alphabet: '0123456789abcdefghijklmnopqrstuvwxyz',
 
-  // function hidecurse(s) {o='';for(i=0; i<s.length; i++){o+='\\x'+(s.charCodeAt(i).toString(16).toUpperCase())} return o; }
-  curses: [
-    '\x66\x75\x63\x6B',
-    '\x73\x68\x69\x74',
-    '\x70\x69\x73\x73',
-    '\x63\x75\x6E\x74',
-    '\x74\x69\x74\x73',
-    '\x6E\x69\x67\x67\x65\x72',
-    '\x63\x6F\x63\x6B\x73\x75\x63\x6B\x65\x72',
-    '\x6D\x6F\x74\x68\x65\x72\x66\x75\x63\x6B\x65\x72',
-    '\\x66\\x72\\x61\\x63\\x6B'
-  ],
+    // function hidecurse(s) {o='';for(i=0; i<s.length; i++){o+='\\x'+(s.charCodeAt(i).toString(16).toUpperCase())} return o; }
+    curses: [
+        '\x66\x75\x63\x6B',
+        '\x73\x68\x69\x74',
+        '\x70\x69\x73\x73',
+        '\x63\x75\x6E\x74',
+        '\x74\x69\x74\x73',
+        '\x6E\x69\x67\x67\x65\x72',
+        '\x63\x6F\x63\x6B\x73\x75\x63\x6B\x65\x72',
+        '\x6D\x6F\x74\x68\x65\x72\x66\x75\x63\x6B\x65\x72',
+        '\\x66\\x72\\x61\\x63\\x6B',
+        '\\x66\\x72\\x61\\x6B'
+    ],
 
 }
-    
 
-var default_cursed = cursing( defaults.curses )
 
-function cursing(curses) {
-  var typestr = Object.prototype.toString.call(curses).substring(8)
+var default_cursed = cursing(defaults.curses)
 
-  if( 'String]' == typestr ) {
-    curses = curses.split(/\s*,\s*/)
-  }
+    function cursing(curses) {
+        var typestr = Object.prototype.toString.call(curses).substring(8)
 
-  if( Array.isArray(curses) ) {
-    return function(code) {
-      var codelower = code.toLowerCase()
-      for( var i = 0; i < curses.length; i++ ) {
-        if( -1 != codelower.indexOf(curses[i]) ) {
-          return true
+        if ('String]' == typestr) {
+            curses = curses.split(/\s*,\s*/)
         }
-      }
+
+        if (Array.isArray(curses)) {
+            return function (code) {
+                var codelower = code.toLowerCase()
+                for (var i = 0; i < curses.length; i++) {
+                    if (-1 != codelower.indexOf(curses[i])) {
+                        return true
+                    }
+                }
+            }
+        } else {
+            if ('Function]' == typestr) {
+                return curses
+            } else if ('RegExp]' == typestr) {
+                return function (code) {
+                    return !!code.match(curses)
+                }
+            } else return function () {
+                return false
+            }
+        }
     }
-  }
-  else {
-    if( 'Function]' == typestr ) {
-      return curses
+
+
+    function generate(opts) {
+        var length = defaults.length
+        var alphabet = defaults.alphabet
+        var cursed = default_cursed
+
+        if (opts) {
+            length = opts.length || length
+            alphabet = opts.alphabet || alphabet
+            cursed = opts.curses ? cursing(opts.curses) : cursed
+        }
+
+        var code = null,
+            numchars = alphabet.length
+
+            do {
+                var time = new Date().getTime()
+                var sb = []
+                for (var i = 0; i < length; i++) {
+                    var c = Math.floor((time * Math.random()) % numchars)
+                    sb.push(alphabet[c])
+                }
+                code = sb.join('')
+            }
+            while (cursed(code))
+
+            return code
     }
-    else if( 'RegExp]' == typestr ) {
-      return function(code) {
-        return !!code.match(curses)
-      }
+
+
+    function make(opts) {
+        ['length', 'alphabet', 'curses'].forEach(function (setting) {
+            opts[setting] = (void 0 === opts[setting]) ? defaults[setting] : opts[setting]
+        })
+
+        if (opts.hex) {
+            opts.alphabet = '0123456789abcdef'
+        } else if (opts.HEX) {
+            opts.alphabet = '0123456789ABCDEF'
+        }
+
+        // exclude overrides curses
+        opts.curses = opts.exclude || opts.curses
+
+        return function () {
+            return generate(opts)
+        }
     }
-    else return function(){return false}
-  }
-}
 
 
-function generate(opts) {
-  var length   = defaults.length
-  var alphabet = defaults.alphabet
-  var cursed   = default_cursed
+    function nid() {
+        var arg0 = arguments[0]
+        if (arg0) {
+            var typestr = Object.prototype.toString.call(arg0).substring(8)
 
-  if( opts ) {
-    length   = opts.length || length
-    alphabet = opts.alphabet || alphabet
-    cursed   = opts.curses ? cursing(opts.curses) : cursed
-  }
+            if ("Number]" === typestr) {
+                return generate({
+                    length: arg0
+                })
+            } else if ("Object]" === typestr) {
+                return make(arg0)
+            } else return generate();
+        }
 
-  var code = null, numchars = alphabet.length
 
-  do { 
-    var time = new Date().getTime()
-    var sb = []
-    for(var i = 0; i < length; i++) {
-      var c = Math.floor((time * Math.random()) % numchars)
-      sb.push( alphabet[c] ) 
+        return generate()
     }
-    code = sb.join('')
-  }
-  while( cursed(code) )
-
-  return code
-}
-
-
-function make(opts) {
-  ['length','alphabet','curses'].forEach( function(setting) {
-    opts[setting] = (void 0 === opts[setting]) ? defaults[setting] : opts[setting]
-  })
-
-  if( opts.hex ) {
-    opts.alphabet = '0123456789abcdef'
-  }
-  else if( opts.HEX ) {
-    opts.alphabet = '0123456789ABCDEF'
-  }
-
-  // exclude overrides curses
-  opts.curses = opts.exclude || opts.curses
-
-  return function() {
-    return generate(opts)
-  }
-}
-
-
-function nid() {
-  var arg0 = arguments[0]
-  if( arg0 ) {
-    var typestr = Object.prototype.toString.call( arg0 ).substring(8)
-    
-    if( "Number]"===typestr ) {
-      return generate({length:arg0})
-    }
-    else if( "Object]"===typestr ) {
-      return make(arg0)  
-    }
-    else return generate();
-  }
-
-
-  return generate()
-}
 
 nid.__defaults = defaults
 
 
 module.exports = nid
-
