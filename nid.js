360114af2283c98abb8ce9221d2b5e56e17ebb0c
rjrodger <richard@ricebridge.com>
2020-02-22 16:20:01 +0000

    deps; expose curses

--- nid.js
+++ nid.js
@@ -1,6 +1,5 @@
 /* Copyright (c) 2013 Richard Rodger, MIT License */
-"use strict";
-
+'use strict'
 
 /* Module API:
  * var nid = require('nid')
@@ -19,135 +18,132 @@
  *
  */
 
-
 var defaults = {
-    length: 6,
-    alphabet: '0123456789abcdefghijklmnopqrstuvwxyz',
+  length: 6,
+  alphabet: '0123456789abcdefghijklmnopqrstuvwxyz',
 
-    // function hidecurse(s) {o='';for(i=0; i<s.length; i++){o+='\\'+'x'+(s.charCodeAt(i).toString(16).toUpperCase())} console.log(o); }
-    curses: [
-        '\x66\x75\x63\x6B',
-        '\x73\x68\x69\x74',
-        '\x70\x69\x73\x73',
-        '\x63\x75\x6E\x74',
-        '\x74\x69\x74\x73',
-        '\x6E\x69\x67\x67\x65\x72',
-        '\x63\x6F\x63\x6B\x73\x75\x63\x6B\x65\x72',
-        '\x6D\x6F\x74\x68\x65\x72\x66\x75\x63\x6B\x65\x72',
-        '\x66\x72\x61\x63\x6B',
-        '\x66\x72\x61\x6B',
-        '\x62\x69\x74\x63\x68',
-        '\x61\x73\x73\x68\x6F\x6C\x65',
-        '\x77\x68\x6F\x72\x65',
-        '\x63\x6F\x63\x6B',
-        '\x74\x77\x61\x74',
-        '\x77\x74\x6E\x6B',
-        '\x73\x6C\x75\x74'
-    ],
-
+  // function hidecurse(s) {o='';for(i=0; i<s.length; i++){o+='\\'+'x'+(s.charCodeAt(i).toString(16).toUpperCase())} console.log(o); }
+  curses: [
+    '\x66\x75\x63\x6B',
+    '\x73\x68\x69\x74',
+    '\x70\x69\x73\x73',
+    '\x63\x75\x6E\x74',
+    '\x74\x69\x74\x73',
+    '\x6E\x69\x67\x67\x65\x72',
+    '\x63\x6F\x63\x6B\x73\x75\x63\x6B\x65\x72',
+    '\x6D\x6F\x74\x68\x65\x72\x66\x75\x63\x6B\x65\x72',
+    '\x66\x72\x61\x63\x6B',
+    '\x66\x72\x61\x6B',
+    '\x62\x69\x74\x63\x68',
+    '\x61\x73\x73\x68\x6F\x6C\x65',
+    '\x77\x68\x6F\x72\x65',
+    '\x63\x6F\x63\x6B',
+    '\x74\x77\x61\x74',
+    '\x77\x74\x6E\x6B',
+    '\x73\x6C\x75\x74'
+  ]
 }
-
 
 var default_cursed = cursing(defaults.curses)
 
-    function cursing(curses) {
-        var typestr = Object.prototype.toString.call(curses).substring(8)
+function cursing(curses) {
+  var typestr = Object.prototype.toString.call(curses).substring(8)
 
-        if ('String]' == typestr) {
-            curses = curses.split(/\s*,\s*/)
-        }
+  if ('String]' == typestr) {
+    curses = curses.split(/\s*,\s*/)
+  }
 
-        if (Array.isArray(curses)) {
-            return function (code) {
-                var codelower = code.toLowerCase()
-                for (var i = 0; i < curses.length; i++) {
-                    if (-1 != codelower.indexOf(curses[i])) {
-                        return true
-                    }
-                }
-            }
-        } else {
-            if ('Function]' == typestr) {
-                return curses
-            } else if ('RegExp]' == typestr) {
-                return function (code) {
-                    return !!code.match(curses)
-                }
-            } else return function () {
-                return false
-            }
+  if (Array.isArray(curses)) {
+    return function(code) {
+      var codelower = code.toLowerCase()
+      for (var i = 0; i < curses.length; i++) {
+        if (-1 != codelower.indexOf(curses[i])) {
+          return true
         }
+      }
     }
+  } else {
+    if ('Function]' == typestr) {
+      return curses
+    } else if ('RegExp]' == typestr) {
+      return function(code) {
+        return !!code.match(curses)
+      }
+    } else
+      return function() {
+        return false
+      }
+  }
+}
 
+function generate(opts) {
+  var length = defaults.length
+  var alphabet = defaults.alphabet
+  var cursed = default_cursed
 
-    function generate(opts) {
-        var length = defaults.length
-        var alphabet = defaults.alphabet
-        var cursed = default_cursed
+  if (opts) {
+    length = opts.length || length
+    alphabet = opts.alphabet || alphabet
+    cursed = opts.curses ? cursing(opts.curses) : cursed
+  }
 
-        if (opts) {
-            length = opts.length || length
-            alphabet = opts.alphabet || alphabet
-            cursed = opts.curses ? cursing(opts.curses) : cursed
-        }
+  var code = null,
+    numchars = alphabet.length
 
-        var code = null,
-            numchars = alphabet.length
-
-            do {
-                var time = new Date().getTime()
-                var sb = []
-                for (var i = 0; i < length; i++) {
-                    var c = Math.floor((time * Math.random()) % numchars)
-                    sb.push(alphabet[c])
-                }
-                code = sb.join('')
-            }
-            while (cursed(code))
-
-            return code
+  do {
+    var time = new Date().getTime()
+    var sb = []
+    for (var i = 0; i < length; i++) {
+      var c = Math.floor((time * Math.random()) % numchars)
+      sb.push(alphabet[c])
     }
+    code = sb.join('')
+  } while (cursed(code))
 
+  return code
+}
 
-    function make(opts) {
-        ['length', 'alphabet', 'curses'].forEach(function (setting) {
-            opts[setting] = (void 0 === opts[setting]) ? defaults[setting] : opts[setting]
-        })
+function make(opts) {
+  ;['length', 'alphabet', 'curses'].forEach(function(setting) {
+    opts[setting] = void 0 === opts[setting] ? defaults[setting] : opts[setting]
+  })
 
-        if (opts.hex) {
-            opts.alphabet = '0123456789abcdef'
-        } else if (opts.HEX) {
-            opts.alphabet = '0123456789ABCDEF'
-        }
+  if (opts.hex) {
+    opts.alphabet = '0123456789abcdef'
+  } else if (opts.HEX) {
+    opts.alphabet = '0123456789ABCDEF'
+  }
 
-        // exclude overrides curses
-        opts.curses = opts.exclude || opts.curses
+  // exclude overrides curses
+  opts.curses = opts.exclude || opts.curses
 
-        return function () {
-            return generate(opts)
-        }
-    }
+  var nid = function nid() {
+    return generate(opts)
+  }
 
+  nid.curses = opts.curses
+  
+  return nid
+}
 
-    function nid() {
-        var arg0 = arguments[0]
-        if (arg0) {
-            var typestr = Object.prototype.toString.call(arg0).substring(8)
+function nid() {
+  var arg0 = arguments[0]
+  if (arg0) {
+    var typestr = Object.prototype.toString.call(arg0).substring(8)
 
-            if ("Number]" === typestr) {
-                return generate({
-                    length: arg0
-                })
-            } else if ("Object]" === typestr) {
-                return make(arg0)
-            } else return generate();
-        }
+    if ('Number]' === typestr) {
+      return generate({
+        length: arg0
+      })
+    } else if ('Object]' === typestr) {
+      return make(arg0)
+    } else return generate()
+  }
 
-
-        return generate()
-    }
+  return generate()
+}
 
 nid.__defaults = defaults
-
+nid.curses = defaults.curses
 
 module.exports = nid
--- test/nid.test.js
+++ test/nid.test.js
@@ -1,8 +1,7 @@
 /* Copyright (c) 2013 Richard Rodger, MIT License */
-"use strict";
+'use strict'
 
 // mocha nid.test.js to test
-
 
 var nid = require('..')
 
@@ -10,163 +9,161 @@ var assert = require('assert')
 
 var defruns = 100000
 
-
-function def_id_fmt_ok( id ) {
+function def_id_fmt_ok(id) {
   return id.match(/^[0-9a-z]{6,6}$/)
 }
 
 function fmt_ok(opts) {
-  if( opts.hex ) {
+  if (opts.hex) {
     opts.alphabet = '0123456789abcdef'
-  }
-  else if( opts.HEX ) {
+  } else if (opts.HEX) {
     opts.alphabet = '0123456789ABCDEF'
   }
 
-  var re = '^['+opts.alphabet+']+$'
+  var re = '^[' + opts.alphabet + ']+$'
   return function(id) {
-    return id.length === (opts.length||6) && id.match(re)
+    return id.length === (opts.length || 6) && id.match(re)
   }
 }
 
-
-function repeat(runs,nidf,passf) {
+function repeat(runs, nidf, passf) {
   var now = new Date().getTime()
   var nids = new Array(runs)
-  for( var i = 0; i < runs; i++) {
+  for (var i = 0; i < runs; i++) {
     nids[i] = nidf()
   }
   var end = new Date().getTime()
-  
-  if( passf ) {
-    for( var i = 0; i < runs; i++) {
-      passf( nids[i] )
+
+  if (passf) {
+    for (var i = 0; i < runs; i++) {
+      passf(nids[i])
     }
   }
 
-  return end-now
+  return end - now
 }
 
-
-function report(name,runs,dur,opts) {
-  console.log( name+': '+Math.floor( runs / (dur/1000) )+' ids per second; '+runs+' runs in '+dur+'ms; len='+opts.length+' A='+opts.alphabet )
+function report(name, runs, dur, opts) {
+  console.log(
+    name +
+      ': ' +
+      Math.floor(runs / (dur / 1000)) +
+      ' ids per second; ' +
+      runs +
+      ' runs in ' +
+      dur +
+      'ms; len=' +
+      opts.length +
+      ' A=' +
+      opts.alphabet
+  )
 }
 
-describe('happy', function(){
-
-  it('works', function(){
+describe('happy', function() {
+  it('works', function() {
     var id = nid()
-    assert.ok( def_id_fmt_ok(id) )
+    assert.ok(def_id_fmt_ok(id))
+    assert.ok(nid.curses.length>0)
   })
 
-
-  it('basic', function(){
-    var dur  = repeat(defruns,nid,def_id_fmt_ok)
-    report('basic',defruns,dur,nid.__defaults)
+  it('basic', function() {
+    var dur = repeat(defruns, nid, def_id_fmt_ok)
+    report('basic', defruns, dur, nid.__defaults)
   })
 
-
-  it('options.length',function(){
-    var opts = {length:8}
+  it('options.length', function() {
+    var opts = { length: 8 }
     var nid8 = nid(opts)
     var id = nid8()
-    assert.ok( fmt_ok(opts)(id) )
+    assert.ok(fmt_ok(opts)(id))
 
     id = nid(8)
-    assert.ok( fmt_ok(opts)(id) )
+    assert.ok(fmt_ok(opts)(id))
+
+    assert.ok(nid8.curses.length>0)
   })
 
-
-  it('options.uuid-ish',function(){
-    var opts = {length:32,hex:true,curses:false}
-    var dur  = repeat(defruns,nid(opts),fmt_ok(opts))
-    report('options.uuid-ish',defruns,dur,opts)
+  it('options.uuid-ish', function() {
+    var opts = { length: 32, hex: true, curses: false }
+    var dur = repeat(defruns, nid(opts), fmt_ok(opts))
+    report('options.uuid-ish', defruns, dur, opts)
   })
 
-
-
-  it('options.alphabet.a',function(){
-    var opts = {length:1,alphabet:'a'}
-    var dur  = repeat(defruns,nid(opts),fmt_ok(opts))
-    report('options.alphabet.a',defruns,dur,opts)
+  it('options.alphabet.a', function() {
+    var opts = { length: 1, alphabet: 'a' }
+    var dur = repeat(defruns, nid(opts), fmt_ok(opts))
+    report('options.alphabet.a', defruns, dur, opts)
   })
 
-
-  it('options.alphabet.ab',function(){
-    var opts = {length:1,alphabet:'ab'}
-    var dur  = repeat(defruns,nid(opts),fmt_ok(opts))
-    report('options.alphabet.ab',defruns,dur,opts)
+  it('options.alphabet.ab', function() {
+    var opts = { length: 1, alphabet: 'ab' }
+    var dur = repeat(defruns, nid(opts), fmt_ok(opts))
+    report('options.alphabet.ab', defruns, dur, opts)
   })
 
-
-  it('options.hex',function(){
-    var opts = {length:1,hex:1}
-    var dur  = repeat(defruns,nid(opts),fmt_ok(opts))
-    report('options.hex',defruns,dur,opts)
+  it('options.hex', function() {
+    var opts = { length: 1, hex: 1 }
+    var dur = repeat(defruns, nid(opts), fmt_ok(opts))
+    report('options.hex', defruns, dur, opts)
   })
 
-
-  it('options.HEX',function(){
-    var opts = {length:1,HEX:1}
-    var dur  = repeat(defruns,nid(opts),fmt_ok(opts))
-    report('options.HEX',defruns,dur,opts)
+  it('options.HEX', function() {
+    var opts = { length: 1, HEX: 1 }
+    var dur = repeat(defruns, nid(opts), fmt_ok(opts))
+    report('options.HEX', defruns, dur, opts)
   })
 
-
-  it('options.curses.array.one',function(){
-    var opts = {length:1,alphabet:'ab',curses:['b']}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'a'}))
-    report('options.curses.array.one',defruns,dur,opts)
+  it('options.curses.array.one', function() {
+    var opts = { length: 1, alphabet: 'ab', curses: ['b'] }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'a' }))
+    report('options.curses.array.one', defruns, dur, opts)
   })
 
-  it('options.exclude.array.one',function(){
-    var opts = {length:1,alphabet:'ab',exclude:['b']}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'a'}))
-    report('options.curses.array.one',defruns,dur,opts)
+  it('options.exclude.array.one', function() {
+    var opts = { length: 1, alphabet: 'ab', exclude: ['b'] }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'a' }))
+    report('options.curses.array.one', defruns, dur, opts)
   })
 
-
-  it('options.curses.array.many',function(){
-    var opts = {length:1,alphabet:'abcd',curses:['b','c']}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'ad'}))
-    report('options.curses.array.many',defruns,dur,opts)
+  it('options.curses.array.many', function() {
+    var opts = { length: 1, alphabet: 'abcd', curses: ['b', 'c'] }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'ad' }))
+    report('options.curses.array.many', defruns, dur, opts)
   })
 
-
-  it('options.curses.csv.one',function(){
-    var opts = {length:1,alphabet:'ab',curses:'b'}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'a'}))
-    report('options.curses.csv.one',defruns,dur,opts)
+  it('options.curses.csv.one', function() {
+    var opts = { length: 1, alphabet: 'ab', curses: 'b' }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'a' }))
+    report('options.curses.csv.one', defruns, dur, opts)
   })
 
-
-  it('options.curses.csv.many',function(){
-    var opts = {length:1,alphabet:'abcd',curses:'b,c'}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'ad'}))
-    report('options.curses.csv.many',defruns,dur,opts)
+  it('options.curses.csv.many', function() {
+    var opts = { length: 1, alphabet: 'abcd', curses: 'b,c' }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'ad' }))
+    report('options.curses.csv.many', defruns, dur, opts)
   })
 
-
-  it('options.curses.re',function(){
-    var opts = {length:1,alphabet:'ab',curses:/b/}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'a'}))
-    report('options.curses.re',defruns,dur,opts)
+  it('options.curses.re', function() {
+    var opts = { length: 1, alphabet: 'ab', curses: /b/ }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'a' }))
+    report('options.curses.re', defruns, dur, opts)
   })
 
-
-  it('options.curses.function',function(){
-    var opts = {length:1,alphabet:'ab',curses:function(id){return 'a'===id}}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'a'}))
-    report('options.curses.function',defruns,dur,opts)
+  it('options.curses.function', function() {
+    var opts = {
+      length: 1,
+      alphabet: 'ab',
+      curses: function(id) {
+        return 'a' === id
+      }
+    }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'a' }))
+    report('options.curses.function', defruns, dur, opts)
   })
 
-
-  it('options.curses.case',function(){
-    var opts = {length:1,alphabet:'aAbB',curses:'b'}
-    var dur  = repeat(defruns,nid(opts),fmt_ok({length:1,alphabet:'aA'}))
-    report('options.curses.case',defruns,dur,opts)
+  it('options.curses.case', function() {
+    var opts = { length: 1, alphabet: 'aAbB', curses: 'b' }
+    var dur = repeat(defruns, nid(opts), fmt_ok({ length: 1, alphabet: 'aA' }))
+    report('options.curses.case', defruns, dur, opts)
   })
-
 })
-
-
